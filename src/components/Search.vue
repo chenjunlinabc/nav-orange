5d47ed36f61bbefcd321f0a4ec9348df30e7cea5
chenjunlin <a@cjlio.com>
2022-10-17 22:43:47 +0800

    处理bug

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,7 +2,7 @@
   <section class="main-search">
     <form method="get" class="search-nav" action="https://cn.bing.com/search" target="_blank" ref="search">
       <input type="text" name="q" ref="btn" class="search-control" placeholder="搜索一下"/>
-      <input type="submit" class="search-submit" value="搜索">
+      <input type="submit" class="search-submit" value="搜索" @click="btnGo()">
     </form>
   </section>
   <div class="search-button">
@@ -30,36 +30,48 @@
 <script>
 export default {
   name: 'Search',
+  watcher:{
+
+  },
   methods:{
+    btnGo(){
+      
+    },
     searchClickb(){
       this.$refs.search.action = 'https://cn.bing.com/search'
       this.$refs.btn.name = 'q'
       this.$refs.btn.placeholder = '必应一下'
+      this.$refs.btn.value = ''
     },
     searchClickg(){
       this.$refs.search.action = 'https://www.google.com/search'
       this.$refs.btn.name = 'q'
       this.$refs.btn.placeholder = '谷歌一下'
+      this.$refs.btn.value = ''
     },
     searchClickd(){
       this.$refs.search.action = 'https://www.baidu.com/s'
       this.$refs.btn.name = 'wd'
       this.$refs.btn.placeholder = '百度一下'
+      this.$refs.btn.value = ''
     },
     searchClickz(){
       this.$refs.search.action = 'https://www.zhihu.com/search'
       this.$refs.btn.name = 'q'
       this.$refs.btn.placeholder = '知乎一下'
+      this.$refs.btn.value = ''
     },
     searchClickbi(){
       this.$refs.search.action = 'https://search.bilibili.com/all'
       this.$refs.btn.name = 'keyword'
       this.$refs.btn.placeholder = 'B站一下'
+      this.$refs.btn.value = ''
     },
     searchClickj(){
       this.$refs.search.action = 'https://juejin.cn/search'
       this.$refs.btn.name = 'query'
       this.$refs.btn.placeholder = '掘金一下'
+      this.$refs.btn.value = ''
     },
 
   }
