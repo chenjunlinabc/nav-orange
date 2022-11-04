723ff7ef02c4fb90c24f3523872c42c662bb7b51
chenjunlin <a@cjlio.com>
2022-11-04 20:11:52 +0800

    push

--- src/components/SearchView.vue
+++ src/components/SearchView.vue
@@ -44,8 +44,75 @@
                 <li>
                     <a href="https://www.cnblogs.com/" rel="noopener noreferrer" target="_blank">博客园</a>
                 </li>
+                <li>
+                    <a href="https://superuser.com/" rel="noopener noreferrer" target="_blank">superuser</a>
+                </li>
             </ul>
         </div>
+        <div class="blog">
+            <div class="box">
+                <h3 class="search-h">大佬博客</h3>
+            </div>
+            <ul>
+                <li>
+                    <a href="http://gityuan.com/" rel="noopener noreferrer" target="_blank">Gityuan</a>
+                </li>
+                <li>
+                    <a href="https://www.liwenzhou.com/" rel="noopener noreferrer" target="_blank">李文周的博客</a>
+                </li>
+                <li>
+                    <a href="https://mofanpy.com/" rel="noopener noreferrer" target="_blank">莫烦Python</a>
+                </li>
+            </ul>
+        </div>
+        <div class="learn">
+            <div class="box">
+                <h3 class="search-h">学习</h3>
+            </div>
+            <ul>
+                <li>
+                    <a href="https://draveness.me/golang/" rel="noopener noreferrer" target="_blank">Go 语言设计与实现</a>
+                </li>
+                <li>
+                    <a href="https://books.studygolang.com/gopl-zh/" rel="noopener noreferrer" target="_blank">Go语言圣经</a>
+                </li>
+                <li>
+                    <a href="https://golang.design/under-the-hood/" rel="noopener noreferrer" target="_blank">Go 语言原本</a>
+                </li>
+                <li>
+                    <a href="https://books.studygolang.com/The-Golang-Standard-Library-by-Example/" rel="noopener noreferrer" target="_blank">Go语言标准库</a>
+                </li>
+                <li>
+                    <a href="https://geektutu.com/post/high-performance-go.html" rel="noopener noreferrer" target="_blank">Go 语言高性能编程</a>
+                </li>
+                <li>
+                    <a href="https://books.studygolang.com/gobyexample/" rel="noopener noreferrer" target="_blank">Go by Example 中文</a>
+                </li>
+                <li>
+                    <a href="https://hao.golangstack.com/" rel="noopener noreferrer" target="_blank">Golang导航</a>
+                </li>
+                <li>
+                    <a href="https://www.topgoer.com/" rel="noopener noreferrer" target="_blank">Go语言中文文档</a>
+                </li>
+                <li>
+                    <a href="https://jkchao.github.io/typescript-book-chinese/" rel="noopener noreferrer" target="_blank">深入理解 TypeScript</a>
+                </li>
+                <li>
+                    <a href="https://www.bookstack.cn/read/es6-3rd/sidebar.md" rel="noopener noreferrer" target="_blank">ECMAScript 6 标准入门</a>
+                </li>
+                <li>
+                    <a href="https://book-refactoring2.ifmicro.com/" rel="noopener noreferrer" target="_blank">重构 改善既有代码的设计</a>
+                </li>
+                <li>
+                    <a href="https://websec.readthedocs.io/zh/latest/" rel="noopener noreferrer" target="_blank">Web安全学习笔记</a>
+                </li>
+                
+                <li>
+                    <a href="https://ued.party/" rel="noopener noreferrer" target="_blank">我的职业是前端工程师</a>
+                </li>
+            </ul>
+        </div>
+        
         <div class="film">
             <div class="box">
                 <h3 class="search-h">影视</h3>
@@ -108,6 +175,12 @@
                 </li>
                 <li>
                     <a href="https://jujuso.com/" rel="noopener noreferrer" target="_blank">优聚搜</a>
+                </li>
+                <li>
+                    <a href="https://www.netlify.com/" rel="noopener noreferrer" target="_blank">Netlify</a>
+                </li>
+                <li>
+                    <a href="https://vercel.com/" rel="noopener noreferrer" target="_blank">Vercel</a>
                 </li>
             </ul>
         </div>
