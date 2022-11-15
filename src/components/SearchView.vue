69305e9963f30c71396b3045f2e678ba81627ec4
chenjunlin <a@cjlio.com>
2022-11-15 22:53:50 +0800

    update test homeurl

--- src/components/SearchView.vue
+++ src/components/SearchView.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="search-view">
+        <div class="other">
+            <div class="box">
+                <h3 class="homeNav">我的导航</h3>
+                <span class="revise" @click="rmNav()">编辑</span>
+            </div>
+            
+            <ul>
+                <li v-for="navTO in hallo" class="Homenav">
+                    <a v-bind:href="navTO.url"  rel="noopener noreferrer" target="_blank">{{ navTO.name }}</a>
+                </li>
+                <li>
+                    <span class="AddTo" @click="AddToNav()">+</span>
+                </li>
+                
+            </ul>
+        </div>
         <div class="tech">
             <div class="box">
                 <h3 class="search-h">技术</h3>
@@ -49,69 +65,6 @@
                 </li>
             </ul>
         </div>
-        <div class="blog">
-            <div class="box">
-                <h3 class="search-h">大佬博客</h3>
-            </div>
-            <ul>
-                <li>
-                    <a href="http://gityuan.com/" rel="noopener noreferrer" target="_blank">Gityuan</a>
-                </li>
-                <li>
-                    <a href="https://www.liwenzhou.com/" rel="noopener noreferrer" target="_blank">李文周的博客</a>
-                </li>
-                <li>
-                    <a href="https://mofanpy.com/" rel="noopener noreferrer" target="_blank">莫烦Python</a>
-                </li>
-            </ul>
-        </div>
-        <div class="learn">
-            <div class="box">
-                <h3 class="search-h">学习</h3>
-            </div>
-            <ul>
-                <li>
-                    <a href="https://draveness.me/golang/" rel="noopener noreferrer" target="_blank">Go 语言设计与实现</a>
-                </li>
-                <li>
-                    <a href="https://books.studygolang.com/gopl-zh/" rel="noopener noreferrer" target="_blank">Go语言圣经</a>
-                </li>
-                <li>
-                    <a href="https://golang.design/under-the-hood/" rel="noopener noreferrer" target="_blank">Go 语言原本</a>
-                </li>
-                <li>
-                    <a href="https://books.studygolang.com/The-Golang-Standard-Library-by-Example/" rel="noopener noreferrer" target="_blank">Go语言标准库</a>
-                </li>
-                <li>
-                    <a href="https://geektutu.com/post/high-performance-go.html" rel="noopener noreferrer" target="_blank">Go 语言高性能编程</a>
-                </li>
-                <li>
-                    <a href="https://books.studygolang.com/gobyexample/" rel="noopener noreferrer" target="_blank">Go by Example 中文</a>
-                </li>
-                <li>
-                    <a href="https://hao.golangstack.com/" rel="noopener noreferrer" target="_blank">Golang导航</a>
-                </li>
-                <li>
-                    <a href="https://www.topgoer.com/" rel="noopener noreferrer" target="_blank">Go语言中文文档</a>
-                </li>
-                <li>
-                    <a href="https://jkchao.github.io/typescript-book-chinese/" rel="noopener noreferrer" target="_blank">深入理解 TypeScript</a>
-                </li>
-                <li>
-                    <a href="https://www.bookstack.cn/read/es6-3rd/sidebar.md" rel="noopener noreferrer" target="_blank">ECMAScript 6 标准入门</a>
-                </li>
-                <li>
-                    <a href="https://book-refactoring2.ifmicro.com/" rel="noopener noreferrer" target="_blank">重构 改善既有代码的设计</a>
-                </li>
-                <li>
-                    <a href="https://websec.readthedocs.io/zh/latest/" rel="noopener noreferrer" target="_blank">Web安全学习笔记</a>
-                </li>
-                
-                <li>
-                    <a href="https://ued.party/" rel="noopener noreferrer" target="_blank">我的职业是前端工程师</a>
-                </li>
-            </ul>
-        </div>
         
         <div class="film">
             <div class="box">
@@ -136,6 +89,7 @@
                 <li>
                     <a href="https://www.huya.com/" rel="noopener noreferrer" target="_blank">虎牙直播</a>
                 </li>
+
             </ul>
         </div>
         <div class="tools">
@@ -182,6 +136,9 @@
                 <li>
                     <a href="https://vercel.com/" rel="noopener noreferrer" target="_blank">Vercel</a>
                 </li>
+                <li>
+                    <a href="https://cupfox.app/" rel="noopener noreferrer" target="_blank">茶杯狐</a>
+                </li>
             </ul>
         </div>
         <div class="info">
@@ -217,6 +174,9 @@
                 <li>
                     <a href="https://tieba.baidu.com/" rel="noopener noreferrer" target="_blank">百度贴吧</a>
                 </li>
+                <li>
+                    <a href="https://cjlio.com/" rel="noopener noreferrer" target="_blank">小陈的辣鸡屋</a>
+                </li>
             </ul>
         </div>
     </div>
@@ -224,6 +184,54 @@
 <script>
 export default {
   name: 'SearchView',
+  methods: {
+      AddToNav(){
+          localStorage.setItem("Links", JSON.stringify(
+            [
+                {
+                    'name': '小陈的辣鸡屋',
+                    'url': 'https://cjlio.com'
+                }, {
+                    'name': '小陈的个人博客',
+                    'url': 'https://blog.cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  }, {
+                      'name': '小陈的辣鸡屋',
+                      'url': 'https://cjlio.com'
+                  },
+            ]
+          ))
+      },
+      rmNav(){
+          localStorage.removeItem("小陈的辣鸡屋")
+      }
+  },
+  data(){
+    return{
+        hallo: JSON.parse(localStorage.getItem('Links'))
+    }
+  },
 }
+console.log(JSON.parse(localStorage.getItem('Links')))
 
 </script>
